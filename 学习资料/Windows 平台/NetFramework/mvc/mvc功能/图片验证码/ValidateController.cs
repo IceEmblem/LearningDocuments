1b417c33624c18a6fa3d97fd9eebdc30eb3739ba
chen7739380547 <1373611035@qq.com>
2020-09-25 01:25:41 +0800

    初始化

---
+++ 学习资料/Windows 平台/NetFramework/mvc/mvc功能/图片验证码/ValidateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using 我的网站.Validate;
+
+namespace 我的网站.Controllers.Validate
+{
+    public class ValidateController : Controller
+    {
+        // 验证码放在Session["ValidateCode"]
+        public ActionResult GetPicValidateCode()
+        {
+            ValidateCode vCode = new ValidateCode();
+            string code = vCode.CreateValidateCode(5);
+            Session["ValidateCode"] = code;
+            byte[] bytes = vCode.CreateValidateGraphic(code);
+            return File(bytes, @"image/jpeg");
+        }
+    }
+}
